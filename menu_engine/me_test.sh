025bc6c0a01b147ac1d15eaf794f6f2dc19b861e
Keith Paterson <keith.w.paterson@gmail.com>
2024-11-22 12:29:43 -0500

    add example with self install logic

--- menu_engine/me_test.sh
+++ menu_engine/me_test.sh
@@ -1,4 +1,7 @@
 _my_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Run `me_test.sh` from the checked out folder only.
+# To test the "self-install" feature run `me_test_se.sh` instead
 ME_CATEGORY_DIR=${_my_dir}/tests
 source ${_my_dir}/menu_engine.sh
 
